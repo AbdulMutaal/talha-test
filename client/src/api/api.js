9d6274eb4e7cb9fb29bb3e9961d7a9bb79404fa9
AbdulMutaal <abdulmutaalazhar@gmail.com>
2022-03-24 20:29:06 +0500

    fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,48 +2,48 @@ import axios from "axios";
 
 export default {
     registerUser: function (userData) {
-        return axios.post("/api/user/register", userData);
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/register", userData);
     },
     loginUser: function (userData) {
-        return axios.post("/api/user/login", userData);
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/login", userData);
     },
     forgot : function(userData){
-        return axios.post("/api/user/forgot",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/forgot",userData)
     },
     changePassword : function(userData){
-        return axios.post("/api/user/changePassword",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/changePassword",userData)
     },
     verifyEmail : function(userData){
-        return axios.post("/api/user/verifyEmail",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/verifyEmail",userData)
     },
     sendVerificationEmail:function(userData){
-        return axios.post("/api/user/sendVerificationEmail",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/sendVerificationEmail",userData)
     },
     loadWallet:function(userData){
-        return axios.post("/api/user/loadWallet",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/loadWallet",userData)
     },
     getProducts:function(){
-        return axios.get("/api/products")
+        return axios.get("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/products")
     },
     placeOrder:function(orderData){
-        return axios.post("/api/order/create",orderData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/order/create",orderData)
     },
     getOrders:function(userData){
-        return axios.post("/api/order",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/order",userData)
     },
     refundOrder:function(userData){
-        return axios.post("/api/order/refund",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/order/refund",userData)
     },
     getCustomers:function(userData){
-        return axios.post("/api/user",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user",userData)
     },
     getWallet:function(userData){
-        return axios.post("/api/user/getWallet",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/getWallet",userData)
     },
     deleteUser:function(userData){
-        return axios.post("/api/user/delete",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/user/delete",userData)
     },
     addBalance:function(userData){
-        return axios.post("/api/deposit/balance",userData)
+        return axios.post("https://t1a2l3h4a5t5e3st1223.herokuapp.com/api/deposit/balance",userData)
     },
 }
